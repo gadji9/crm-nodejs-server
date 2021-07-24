252b7d30928874985eaff4d75ab0f909d14869a0
Gadjimurad <78681462+gadji9@users.noreply.github.com>
2021-07-24 14:26:44 +0300

    Deleted

--- server/transliter.js
+++
@@ -1,8 +0,0 @@
-const translit = require('cyrillic-to-translit-js')
-const trans = new translit()
-
-
-
-module.exports = (word) =>{
-    return trans.transform(word.toLowerCase())
-}
--- server/transliterrevers.js
+++
@@ -1,6 +0,0 @@
-const translit = require('cyrillic-to-translit-js')
-const trans = new translit()
-
-module.exports = (word) => {
-    return trans.reverse(word)
-}
